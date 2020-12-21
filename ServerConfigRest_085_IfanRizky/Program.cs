16e66c9092667c89ca6f4f2d8ecce1cf8bde9f32
Mochammad Ifan Rizky Kurniawan <62989237+IfanRizky@users.noreply.github.com>
2020-12-21 09:39:18 +0700

    Add project files.

---
+++ ServerConfigRest_085_IfanRizky/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using ServiceRest_085_IfanRizky;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConfigRest_085_IfanRizky
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server Ready");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
